1e580ddb818c217f85e67cb5a0fd90ae0fdeec83
Oleksandr Yurchenko <rockysfairat@gmail.com>
2021-04-23 13:14:28 +0200

    testing API call

--- src/Pages/ArtistPage/ArtistPage.jsx
+++ src/Pages/ArtistPage/ArtistPage.jsx
@@ -1,43 +1,55 @@
 import './artistpage.css';
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 import artistPlaceholderImg from '../../images/artistPlaceholder.jpg';
 
 import Footer from '../../Components/Footer/Footer.jsx';
+import React from 'react';
 
 export default function ArtistPage() {
     return <div className='PageContainer'>
         <h2>Genre: one of three </h2>
         <div className='artistPicker'>
             <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
-            <Artist />
         </div>
         <Footer />
     </div>
 }
 
-const artistNameAndImg = {
-    name: 'Test name',
-    placeholder: artistPlaceholderImg
-}
 
-const Artist = (props) => {
-    const { name, placeholder } = artistNameAndImg;
-    return <div className='artistContainer'>
-        <div className='artistPhoto'>
-            <img src={artistNameAndImg.placeholder} />
+
+const Artist = () => {
+    const [artistList, setArtistList] = useState([]);
+
+    useEffect( () => {
+        async function fetchArtistData() {
+            try {
+                const apiCallResponse = await axios.get('https://api.deezer.com/search?q=drumandbass');
+                setArtistList(apiCallResponse.data);
+            } catch (error) {
+                console.log('This is the error!');
+            }
+        }
+        fetchArtistData();
+    }, [])
+
+    if (artistList === 0) {
+        return 'Sorrrrrrryyyyyyy!!!!';
+    }
+
+    return (
+        <div className='artistContainer'>
+            {artistList.map( (artistItem) => {
+                return ( <React.Fragment>
+                    
+                    <div className='artistPhoto'>
+                        <img src={artistItem.artist.picture_medium} />
+                    </div>
+                    <h3>{artistItem.title}</h3>
+                </React.Fragment> )
+            } ) }
         </div>
-        <h3>{artistNameAndImg.name}</h3>
-    </div>
+    )
 }
