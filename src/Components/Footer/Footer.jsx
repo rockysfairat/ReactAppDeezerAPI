391ec973cf8530238eff08ed6812754dddb6b22b
Oleksandr Yurchenko <rockysfairat@gmail.com>
2021-05-14 13:19:15 +0200

    Updates

--- src/Components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import './footer.css';
-import DeezerLogo from '../../images/DeezerLogo.png';
-
-export default function Footer() {
-    return <footer>
-        Powered by <img className='deezer_logo' src={DeezerLogo} alt="Deezer logo" />
-    </footer>
-}
--- src/Components/No_Artist_Data/NoArtistData.jsx
+++
@@ -1,9 +0,0 @@
-import './no_artist_data.css';
-import tumbleweed from '../../images/tumbleweed.gif';
-
-export default function Loader () {
-    return <div className='noArtistData_container'>
-        <img className='noArtistData_gif' src={tumbleweed} alt="" />
-        <h2>Sorry, but now there's no data about this artist :( </h2>
-    </div>
-}
